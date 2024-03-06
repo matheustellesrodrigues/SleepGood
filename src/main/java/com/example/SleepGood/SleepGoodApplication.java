248a3979ba1ad6fb6c5d34ab5d14aaaabfda24e6
matheustellesrodrigues <matheustelles888ian@gmail.com>
2024-03-06 09:14:23 -0300

    inicializar projeto

---
+++ src/main/java/com/example/SleepGood/SleepGoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.SleepGood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SleepGood/SleepGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SleepGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
