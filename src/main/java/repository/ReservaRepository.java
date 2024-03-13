8656df57859664f3773dbc923c8bf035564cbdd9
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 10:47:38 -0300

    Modificacoes

--- src/main/java/repository/ReservaRepository.java
+++ src/main/java/repository/ReservaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.SleepGood.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    // Se necessário, adicione métodos personalizados aqui
+    
 }
