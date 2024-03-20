0e613a5d40d52f8f5e61382e30455e39d15b0ce9
matheustellesrodrigues <matheustelles888ian@gmail.com>
2024-03-19 22:36:47 -0300

    Atualizações

---
+++ SleepGood-main/src/main/java/repository/ReservaRepository.java
@@ -0,0 +1,8 @@
+package com.example.SleepGood.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.SleepGood.model.Reserva;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    
+}
